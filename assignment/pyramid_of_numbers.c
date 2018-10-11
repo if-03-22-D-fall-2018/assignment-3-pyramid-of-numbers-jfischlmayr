9f0e113dae08fd41f48da175edf5c5ccdc05e15d
jfischlmayr <jan.fischlmayr@gmx.net>
2018-10-11 11:18:10 +0200

    Added some methods

--- assignment/pyramid_of_numbers.c
+++ assignment/pyramid_of_numbers.c
@@ -14,6 +14,7 @@
  */
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 /// The maximum number of digits allowed in a big int.
 #define MAX_DIGITS 80
@@ -69,6 +70,8 @@ void copy_big_int(const struct BigInt *from, struct BigInt *to);
 /** Asks the user for his BigInt*/
 void get_user_input(char* inputNumber);
 
+void bigintarray_reverse(struct BigInt *big_int);
+
 /**
 *** main() reads the base number from which the pyramid has to be calculated
 *** into an array of char. The max. length of this number is MAX_DIGITS.
@@ -85,28 +88,53 @@ int main(int argc, char *argv[]) {
 	int lengthOfCharArray;
 	struct BigInt bigInt;
 	struct BigInt bigResult;
+	struct BigInt bigResultDivide;
 
 	printf("Pyramid of Numbers\n");
 	printf("==================\n");
 	get_user_input(inputNumber);
 	lengthOfCharArray = strlen(inputNumber);
-	int bigIntLength = strtobig_int(inputNumber, lengthOfCharArray, &bigInt);
-	for (int i = 2; i < 10; i++) {
-		multiply(&bigInt, i, &bigResult);
-		printf("%d * %d = %d\n", bigInt.the_int[i - 2], i , bigResult.the_int[i - 2]);
+	int length = strtobig_int(inputNumber, lengthOfCharArray, &bigInt);
+
+	if (length == 0)
+	{
+		return 0;
 	}
 
+	for (int i = 2; i < 10; i++) {
+		multiply(&bigInt, i, &bigResult);
+		bigintarray_reverse(&bigResult);
+		bigintarray_reverse(&bigInt);
+		print_big_int(&bigInt);
+		printf(" * %d = ",i);
+		print_big_int(&bigResult);
+		printf("\n");
+		bigintarray_reverse(&bigResult);
+		bigintarray_reverse(&bigInt);
+		bigInt = bigResult;
+	}
+
+	bigintarray_reverse(&bigResult);
+
+	for (int i = 2; i < 10; i++) {
+		print_big_int(&bigResult);
+		divide(&bigResult,i,&bigResultDivide);
+		bigResult = bigResultDivide;
+		printf(" / %d = ",i);
+		print_big_int(&bigResultDivide);
+		printf("\n");
+}
 	return 0;
 }
 
 void get_user_input(char* inputNumber) {
-	printf("Enter your Number: \n");
+	printf("Enter your Number: ");
 	scanf("%s\n", inputNumber);
 }
 
 int strtobig_int(const char *str, int len, struct BigInt *big_int) {
 	int counter = 0;
-	for (size_t i = 0; i < len; i++) {
+	for (int i = 0; i < len; i++) {
 		big_int->the_int[i] = str[i] - '0';
 		counter++;
 	}
@@ -114,20 +142,69 @@ int strtobig_int(const char *str, int len, struct BigInt *big_int) {
 }
 
 void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result) {
-	int overflowNumber=0;
-	for (int i = big_int->digits_count-1 ; i >0; i++) {
-		int tempResult=big_int->the_int[i]*factor;
+	int overflowNumber = 0;
+	int tempResult = 0;
+	for (int i = big_int->digits_count - 1 ; i > 0; i--) {
+		tempResult = big_int->the_int[i] * factor + overflowNumber;
 
 		if (tempResult>9) {
-				big_result->the_int[i]=tempResult%10+overflowNumber;
-				overflowNumber=tempResult/10;
+			if (i == big_int->digits_count-1) {
+				big_result->digits_count++;
+				big_result->the_int[i] = tempResult % 10;
+				big_result->the_int[i + 1] = tempResult / 10;
+			}
+			else {
+				overflowNumber = tempResult / 10;
+				big_result->the_int[i] = tempResult % 10;
+			}
 		}
 		else{
-			big_result->the_int[i]=tempResult+overflowNumber;
+			big_result->the_int[i]=tempResult;
 		}
 	}
 }
 
 void divide(const struct BigInt *big_int, int divisor, struct BigInt *big_result) {
-	
+	int overflowNumber = 0;
+	int tempResult;
+	for (int i = 0; i < big_int->digits_count; i++) {
+		big_result->the_int[i] = 0;
+		big_result->digits_count = i + 1;
+		tempResult = overflowNumber * 10 + big_int->the_int[i];
+		if (tempResult >= divisor) {
+			big_result->the_int[i] = tempResult / divisor;
+			overflowNumber = tempResult % divisor;
+		}
+		else
+		{
+				overflowNumber = big_int->the_int[i];
+		}
+	}
+}
+
+void print_big_int(const struct BigInt *big_int)
+{
+	bool notNullDigitFound = false;
+		for (int i = 0; i < big_int->digits_count; i++) {
+			if (big_int->the_int[i] > 0 || notNullDigitFound == true)
+			{
+					printf("%d",big_int->the_int[i]);
+					notNullDigitFound = true;
+			}
+		}
+}
+
+void bigintarray_reverse(struct BigInt *big_int)
+{
+	int i = big_int->digits_count-1;
+  int j = 0;
+   while(i > j)
+   {
+     int temp = big_int->the_int[i];
+		 int temp2 = big_int->the_int[j];
+     big_int->the_int[i] = temp2;
+     big_int->the_int[j] = temp;
+     i--;
+     j++;
+   }
 }
